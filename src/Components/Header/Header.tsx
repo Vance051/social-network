c3c4eac328c2d78fa650d519b1cc7f98461dcc84
IvanVance <vance051@gmail.com>
2023-11-21 23:43:50 +0300

    create Header component

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div clssName='header'>
+            <img src="https://cdn.pixabay.com/photo/2023/09/16/18/18/wallpaper-8257343_1280.png" alt="background"/>
+            </div>
+    );
+};
+
+export default Header;
