0691705aa268f83e436b1ff3ea72521a221210a7
IvanVance <vance051@gmail.com>
2023-12-02 23:58:30 +0300

    21. Make the Dialogs page

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import s from 'Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
-        </div>
+
+            <div className={s.dialogs}>
+
+                <div className={s.dialogItem}>
+                    <div className={`${s.dialog} ${s.active}`}>Ivan</div>
+                    <div className={s.dialog}>Alesya</div>
+                    <div className={s.dialog}>Mashunya</div>
+                    <div className={s.dialog}>Kosty</div>
+                    <div className={s.dialog}>Valera</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hello</div>
+                    <div className={s.message}>Its me</div>
+                    <div className={s.message}>Its snowing</div>
+                </div>
+            </div>
+
     );
 };
 
