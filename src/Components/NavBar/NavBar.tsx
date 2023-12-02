7b8126fa298f53bd0bfa6a041d508278b4461017
IvanVance <vance051@gmail.com>
2023-12-02 12:57:21 +0300

    20. Add Replace <a> to NavLink/ add active style to active NavLink

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import s from './NavBar.module.css'
+import {NavLink} from "react-router-dom";
 const NavBar = () => {
     return (
 
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}>
-                <a href="/profile">Profile</a>
+            <div className={s.item}>
+                <NavLink activeClassName={s.active} to="/profile">Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/dialogs">Dialogs</a>
+                <NavLink activeClassName={s.active} to="/dialogs">Dialogs</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink activeClassName={s.active} to="/news">News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/musics">Music</a>
+                <NavLink activeClassName={s.active} to="/musics">Music</NavLink>
             </div>
             <div className={s.item}>
                 <a href="/#">Settings</a>
