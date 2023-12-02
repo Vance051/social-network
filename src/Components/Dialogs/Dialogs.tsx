7f0ee45a98aafa2f6bcddb0ab08df7ea82d69b63
IvanVance <vance051@gmail.com>
2023-12-03 00:40:38 +0300

    23. refactoring Dialogs page. Add DialogsItem and Message components

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, {FC} from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type PropsDialogItemType = {
+    name: string
+    id: number
+}
+
+type PropsMessageType = {
+    message: string
+}
+
+const DialogItem: FC<PropsDialogItemType> = (props) => {
+    const path = `/dialogs/1${props.id}`
+    return (
+        <div className={`${s.dialog} ${s.active}`}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message: FC<PropsMessageType> = (props) => {
+    return (<div className={s.message}>{props.message}</div>
+    )
+}
 const Dialogs = () => {
     return (
 
-            <div className={s.dialogs}>
+        <div className={s.dialogs}>
 
-                <div className={s.dialogItem}>
-                    <div className={`${s.dialog} ${s.active}`}>
-                        <NavLink to={'/dialogs/1'}>Ivan</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={'/dialogs/2'}>Alesya</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={'/dialogs/3'}>Mashunya</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={'/dialogs/4'}>Kosty</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={'/dialogs/5'}>Valera</NavLink>
-                    </div>
-                </div>
-                <div className={s.messages}>
-                    <div className={s.message}>Hello</div>
-                    <div className={s.message}>Its me</div>
-                    <div className={s.message}>Its snowing</div>
-                </div>
+            <div className={s.dialogItem}>
+                <DialogItem name={'Ivan'} id={1}/>
+                <DialogItem name={'Alesya'} id={1}/>
+                <DialogItem name={'Mashunya'} id={1}/>
+                <DialogItem name={'Kosty'} id={1}/>
+                <DialogItem name={'Valera'} id={1}/>
+
             </div>
+            <div className={s.messages}>
+                <Message message={'Hello'}/>
+                <Message message={'How are u'}/>
+                <Message message={'Let its snow..'}/>
+
+            </div>
+        </div>
 
     );
 };
